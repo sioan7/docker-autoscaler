958cf9caaca1a963c0ede1882c4261f2ae27b361
illihcs <j.schillemans@gmx.de>
2019-12-17 10:07:20 +0100

    init Java implementation for numberworker

---
+++ numberworker/src/main/java/numberworker.java
@@ -0,0 +1,13 @@
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+
+public class numberworker {
+    public numberworker() {
+        MongoClient mongoClient = MongoClients.create();
+    }
+
+    public void findNumbers(){
+
+    }
+}
