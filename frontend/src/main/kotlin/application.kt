3f2ea2b90efbea2db5697356f7cc798e60420b75
Stănilă Ioan <stanila.ionutsss@gmail.com>
2019-12-27 12:13:49 +0200

    optimized imports

--- frontend/src/main/kotlin/application.kt
+++ frontend/src/main/kotlin/application.kt
@@ -1,23 +1,18 @@
 import businesslogic.processUploadedFile
 import io.ktor.application.Application
-import io.ktor.application.ApplicationCall
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
 import io.ktor.gson.gson
 import io.ktor.html.respondHtml
-import io.ktor.http.HttpStatusCode
 import io.ktor.http.content.*
-import io.ktor.request.receiveMultipart
-import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import io.ktor.util.pipeline.PipelineContext
 import pages.index
 import java.io.*
 
